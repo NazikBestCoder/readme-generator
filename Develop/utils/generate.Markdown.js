f82bf8f878d1721ac95b5e530482e4c802819284
NazikBest Coder <n.askarbekkyzy@gmail.com>
2021-10-07 12:29:15 -0400

    readme generator functions

---
+++ Develop/utils/generate.Markdown.js
@@ -0,0 +1,92 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "MIT") {
+      return "![MIT license badge](https://img.shields.io/badge/license-MIT-brightgreen)"
+  } else if (license === "Apache") {
+      return "![Apache license badge](https://img.shields.io/badge/license-Apache-brightgreen)"
+  } else if (license === "ISC") {
+      return "![ISC license badge](https://img.shields.io/badge/license-ISC-brightgreen)"
+  }
+}
+
+function renderLicenseSection(license, year, fullName) {
+  if (license === "MIT") {
+      return `MIT License
+
+      Copyright (c) ${year} ${fullName}
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.`
+          } else if (license === "Apache") {
+              return `Copyright ${year} ${fullName}
+      Licensed under the Apache License, Version 2.0 (the "License");
+      you may not use this file except in compliance with the License.
+      You may obtain a copy of the License at
+      http://www.apache.org/licenses/LICENSE-2.0
+      Unless required by applicable law or agreed to in writing, software
+      distributed under the License is distributed on an "AS IS" BASIS,
+      WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+      See the License for the specific language governing permissions and
+      limitations under the License.`
+          } else if (license === "ISC"){
+              return `ISC License
+      Copyright (c) ${year} ${fullName}
+      Permission to use, copy, modify, and/or distribute this software for any
+      purpose with or without fee is hereby granted, provided that the above
+      copyright notice and this permission notice appear in all copies.
+      THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+      REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+      AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+      INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+      LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+      OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+      PERFORMANCE OF THIS SOFTWARE.`
+          }  
+      }
+      
+
+
+// TODO: Create a function to generate markdown for README
+
+function generateMarkdown(data) {
+  return `# ${data.title}
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [How To Contribute](#how-to-contribute)
+- [Tests](#tests)
+- [Questions](#questions)
+## Installation
+${data.installation}
+## Usage
+${data.usage}
+## License
+${renderLicenseSection(data.license, data.year, data.fullName)}
+## How to Contribute
+${data.contributing}
+## Tests
+${data.tests}
+## Questions
+If you have any question you can view my GitHub Profile [here](https://www.github.com/${data.github}), or you can contact me here: ${data.email}
+`;
+}
+
+module.exports = generateMarkdown;
